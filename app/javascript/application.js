c3440efda5ad4be9fbf8d3f1099ab9ba3a56bd2b
Raimunds <raimunds.vanags@gmail.com>
2024-05-25 00:16:02 +0300

    included alpine js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,18 +2,22 @@
 // app/javascript/packs/application.js    
 //= require jquery
 //= require jquery_ujs
+import Alpine from 'alpinejs'
 
+window.Alpine = Alpine
+Alpine.start()
 document.addEventListener('DOMContentLoaded', function() {
-setTimeout(function() {
-        var noticeElement = document.getElementById('notice');
-        var errorElement = document.getElementById('error');
-        
-        if (noticeElement) {
-          noticeElement.style.display = 'none';
-        }
-        
-        if (errorElement) {
-          errorElement.style.display = 'none';
-        }
-      }, 3000);
-});
+  setTimeout(function() {
+          var noticeElement = document.getElementById('notice');
+          var errorElement = document.getElementById('error');
+          
+          if (noticeElement) {
+            noticeElement.style.display = 'none';
+          }
+          
+          if (errorElement) {
+            errorElement.style.display = 'none';
+          }
+        }, 3000);
+  });
+  
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application', preload: true
+pin "alpinejs" # @3.14.0
